b72107ca02ef0fd140d2dd82aa4ea41209229b6c
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-05-14 22:01:54 +0200

    refactor DataHandler

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,4 +1,21 @@
 package app;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DataHandler {
+
+    // Initializing the ReentrantLock lock object
+    private final Lock lock = new ReentrantLock();
+
+    // The synchronized keyword has been removed since Lock is used
+    public int modify(int num) {
+        lock.lock(); // Acquiring a lock before performing an operation
+        try {
+            num = num * 3;
+            return num;
+        } finally {
+            lock.unlock(); // Mandatory deallocation in the finally block
+        }
+    }
 }
